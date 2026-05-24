279ee44e45ca8f138a5a90dd790508a68ab180aa
Erlin <88038578@qq.com>
2026-05-24 12:12:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const $ = id => document.getElementById(id), TEXT = { zh: ["数据目录", "搜索..."], en: ["Data Directory", "Search..."]};
 const {data, CATA} = await fetch("./data.json").then(r => r.json());
 let isEN = new URLSearchParams(location.search).get("lang") === "en";
-const clean = s => s.replace(/^【.*?】/, ""), hl = (t, k) => k ? t.replace(new RegExp(`(${k.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})`, "ig"), '<span class="h1">$1</span>') : t, txt = v => isEN ? CATA[v] || v : v, name = i => isEN ? i["English-title"] || clean(i.title) : i.title, match = (i, k) => !k || [i.title, i["English-title"], i.cata].some(v => v?.toLowerCase().includes(k.toLowerCase()));
+const clean = s => s.replace(/^【.*?】/, ""), hl = (t, k) => k ? t.replace(new RegExp(`(${k.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})`, "ig"), '<span class="h">$1</span>') : t, txt = v => isEN ? CATA[v] || v : v, name = i => isEN ? i["English-title"] || clean(i.title) : i.title, match = (i, k) => !k || [i.title, i["English-title"], i.cata].some(v => v?.toLowerCase().includes(k.toLowerCase()));
 const render = (k = "") => {
     const map = {};
     data.forEach(i => {
