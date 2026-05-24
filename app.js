f089c2b25c96a1ddfb049907c67e07bc95eb0dbb
Erlin <88038578@qq.com>
2026-05-24 11:30:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const $ = id => document.getElementById(id), TEXT = {
-    zh: ["数据目录", "搜索..."],
-    en: ["Data Directory", "Search..."]
-};
-const {
-    data,
-    CATA
-} = await fetch("./data.json").then(r => r.json());
+const $ = id => document.getElementById(id), TEXT = { zh: ["数据目录", "搜索..."], en: ["Data Directory", "Search..."]};
+const {data, CATA} = await fetch("./data.json").then(r => r.json());
 let isEN = new URLSearchParams(location.search).get("lang") === "en";
 const clean = s => s.replace(/^【.*?】/, ""), hl = (t, k) => k ? t.replace(new RegExp(`(${k.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})`, "ig"), '<span class="h1">$1</span>') : t, txt = v => isEN ? CATA[v] || v : v, name = i => isEN ? i["English-title"] || clean(i.title) : i.title, match = (i, k) => !k || [i.title, i["English-title"], i.cata].some(v => v?.toLowerCase().includes(k.toLowerCase()));
 const render = (k = "") => {
